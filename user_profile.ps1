6aad5a3476bcca1f4e1acc2db1141ef880f5ea5d
frank <frankparedesalpi@gmail.com>
2021-12-18 09:06:57 -0500

    Fisrt commit

---
+++ user_profile.ps1
@@ -0,0 +1,38 @@
+# Prompt
+Import-Module posh-git
+Import-Module oh-my-posh
+
+#Load promt config
+
+$omp_config = Join-Path $PSScriptRoot ".\omp.json"
+oh-my-posh --init --shell pwsh --config $omp_config | Invoke-Expression
+
+Import-Module -Name Terminal-Icons
+
+# PSReadLine
+Set-PSReadLineOption -EditMode Emacs
+Set-PSReadLineOption -BellStyle None
+Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
+Set-PSReadLineOption -PredictionSource History
+
+#Fzf
+Import-Module PSFzf
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
+
+#Env
+$env:GIT_SSH = "C:\Windows\system32\OpenSSH\ssh.exe"
+
+#Utilities
+function which($command){
+  Get-Command -Name $command -ErrorAction SilentlyContinue |
+    Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
+
+# Alias
+Set-Alias vim nvim
+Set-Alias ll ls
+Set-Alias g git
+Set-Alias grep findstr
+#Set-Alias clear cls
+Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
+Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
